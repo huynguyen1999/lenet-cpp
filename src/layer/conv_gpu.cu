d39e437fddeeb3f45ad8b427eda13914f504d6d0
IceH <conghuy.nguyentran.1999@gmail.com>
2024-01-09 15:55:23 +0700

    update conv gpu

--- src/layer/conv_gpu.cu
+++ src/layer/conv_gpu.cu
@@ -1,10 +1,12 @@
 #include "conv_gpu.h"
 #include <math.h>
 #include <iostream>
-#include <chrono>
-#include <vector>
+#include <typeinfo>
+#include <assert.h>
 
+// Largest weights are shape (5 * 5 * 4, 16)
 #define MAX_WEIGHT_SIZE 1600
+
 __constant__ float const_weights[MAX_WEIGHT_SIZE];
 
 void ConvGpu::init()
@@ -12,13 +14,14 @@ void ConvGpu::init()
     height_out = (1 + (height_in - height_kernel + 2 * pad_h) / stride);
     width_out = (1 + (width_in - width_kernel + 2 * pad_w) / stride);
     dim_out = height_out * width_out * channel_out;
-
     weight.resize(channel_in * height_kernel * width_kernel, channel_out);
     bias.resize(channel_out);
     grad_weight.resize(channel_in * height_kernel * width_kernel, channel_out);
     grad_bias.resize(channel_out);
     set_normal_random(weight.data(), weight.size(), 0, 0.01);
     set_normal_random(bias.data(), bias.size(), 0, 0.01);
+    // std::cout << weight.colwise().sum() << std::endl;
+    // std::cout << weight.colwise().sum() + bias.transpose() << std::endl;
 }
 
 void ConvGpu::im2col(const Vector &image, Matrix &data_col)
@@ -30,7 +33,7 @@ void ConvGpu::im2col(const Vector &image, Matrix &data_col)
     data_col.resize(hw_out, hw_kernel * channel_in);
     for (int c = 0; c < channel_in; c++)
     {
-        Vector map = image.block(hw_in * c, 0, hw_in, 1); // c-th channel map
+        Vector map = image.block(hw_in * c, 0, hw_in, 1); // c-th channel map // Block of size (hw_in,1) starting at (hw_in*c, 0)
         for (int i = 0; i < hw_out; i++)
         {
             int step_h = i / width_out;
@@ -47,11 +50,76 @@ void ConvGpu::im2col(const Vector &image, Matrix &data_col)
                 }
                 else
                 {
+                    // int pick_idx = start_idx + (j / width_kernel) * width_in + j % width_kernel;
                     int pick_idx = cur_row * width_in + cur_col;
                     data_col(i, c * hw_kernel + j) = map(pick_idx); // pick which pixel
                 }
             }
         }
+    }
+}
+
+void ConvGpu::forward(const Matrix &bottom)
+{
+    int n_sample = bottom.cols();
+    top.resize(height_out * width_out * channel_out, n_sample);
+    data_cols.resize(n_sample);
+    for (int i = 0; i < n_sample; i++)
+    {
+
+        float *d_image;
+        float *d_results;
+
+        size_t size_image = sizeof(float) * height_in * width_in * channel_in;
+        size_t size_result = sizeof(float) * height_out * width_out * channel_out;
+        size_t size_weight = sizeof(float) * channel_in * height_kernel * width_kernel * channel_out;
+
+        CHECK(cudaMalloc((void **)&d_image, size_image));
+        CHECK(cudaMalloc((void **)&d_results, size_result));
+
+        // Access only the current sample using column-major indexing
+        int image_idx = i * height_in * width_in * channel_in;
+
+        // Copy the image data
+        CHECK(cudaMemcpy(d_image, &bottom.data()[image_idx], size_image, cudaMemcpyHostToDevice));
+        // Copy the weight data
+        CHECK(cudaMemcpyToSymbol(const_weights, weight.data(), size_weight));
+
+        // Define more X threads as there are usually more pixels
+        // than there are channels out
+        int num_threadsX = 256;
+        int num_threadY = 4;
+
+        // Use float to prevent int division
+        float hw_out = height_out * width_out;
+
+        int dimGridSizeX = ceil(hw_out / num_threadsX);
+        int dimGridSizeY = ceil(channel_out / num_threadY);
+
+        dim3 DimGrid(dimGridSizeX, dimGridSizeY, 1);
+        dim3 DimBlock(num_threadsX, num_threadY, 1);
+        // Launch Kernel Using Defined dimensions
+        conv<<<DimGrid, DimBlock>>>(d_image, d_results, height_in, width_in, height_kernel,
+                                    width_kernel, height_out, width_out, channel_in, channel_out, stride, pad_w, pad_h);
+        // Ensure Kernel executed sucessfully
+        CHECK(cudaGetLastError());
+
+        // Copy the result over to a host array
+        float result[height_out * width_out * channel_out];
+        cudaMemcpy(result, d_results, size_result, cudaMemcpyDeviceToHost);
+        // Create an Eigen::Matrix and convert the 1D array to 2D
+        Matrix output = Eigen::Map<Matrix>(result, height_out * width_out, channel_out);
+        // Add bias
+        output.rowwise() += bias.transpose();
+
+        // Flatten the output
+        top.col(i) = Eigen::Map<Vector>(output.data(), output.size());
+
+        // Free Cuda Memory
+        CHECK(cudaFree(d_image));
+        CHECK(cudaFree(d_results));
+        // Check for any Cuda Errrors
+        CHECK(cudaGetLastError());
     }
 }
 
@@ -110,77 +178,9 @@ __global__ void conv(float *image, float *result, int height_in, int width_in, i
     }
 }
 
-void ConvGpu::forward(const Matrix &bottom)
-{
-    GpuTimer timer;
-    timer.Start();
-    int n_sample = bottom.cols();
-    top.resize(height_out * width_out * channel_out, n_sample);
-    data_cols.resize(n_sample);
-    for (int i = 0; i < n_sample; i++)
-    {
-
-        float *d_image;
-        float *d_results;
-
-        size_t size_image = sizeof(float) * height_in * width_in * channel_in;
-        size_t size_result = sizeof(float) * height_out * width_out * channel_out;
-        size_t size_weight = sizeof(float) * channel_in * height_kernel * width_kernel * channel_out;
-
-        CHECK(cudaMalloc((void **)&d_image, size_image));
-        CHECK(cudaMalloc((void **)&d_results, size_result));
-
-        // Access only the current sample using column-major indexing
-        int image_idx = i * height_in * width_in * channel_in;
-
-        // Copy the image data
-        CHECK(cudaMemcpy(d_image, &bottom.data()[image_idx], size_image, cudaMemcpyHostToDevice));
-        // Copy the weight data
-        CHECK(cudaMemcpyToSymbol(const_weights, weight.data(), size_weight));
-
-        // Define more X threads as there are usually more pixels
-        // than there are channels out
-        int num_threadsX = 256;
-        int num_threadY = 4;
-
-        // Use float to prevent int division
-        float hw_out = height_out * width_out;
-
-        int dimGridSizeX = ceil(hw_out / num_threadsX);
-        int dimGridSizeY = ceil(channel_out / num_threadY);
-
-        dim3 DimGrid(dimGridSizeX, dimGridSizeY, 1);
-        dim3 DimBlock(num_threadsX, num_threadY, 1);
-
-        // Launch Kernel Using Defined dimensions
-        conv<<<DimGrid, DimBlock>>>(d_image, d_results, height_in, width_in, height_kernel,
-                                    width_kernel, height_out, width_out, channel_in, channel_out, stride, pad_w, pad_h);
-
-        // Ensure Kernel executed sucessfully
-        CHECK(cudaGetLastError());
-
-        // Copy the result over to a host array
-        float result[height_out * width_out * channel_out];
-        cudaMemcpy(result, d_results, size_result, cudaMemcpyDeviceToHost);
-        // Create an Eigen::Matrix and convert the 1D array to 2D
-        Matrix output = Eigen::Map<Matrix>(result, height_out * width_out, channel_out);
-        // Add bias
-        output.rowwise() += bias.transpose();
-
-        // Flatten the output
-        top.col(i) = Eigen::Map<Vector>(output.data(), output.size());
-
-        // Free Cuda Memory
-        CHECK(cudaFree(d_image));
-        CHECK(cudaFree(d_results));
-        // Check for any Cuda Errrors
-        CHECK(cudaGetLastError());
-    }
-
-    timer.Stop();
-    std::cout << "\t - Layer Time: " << timer.Elapsed() << " ms" << std::endl;
-}
-
+// col2im, used for grad_bottom
+// data_col size: Matrix (hw_out, hw_kernel * channel_in)
+// image size: Vector (height_in * width_in * channel_in)
 void ConvGpu::col2im(const Matrix &data_col, Vector &image)
 {
     int hw_in = height_in * width_in;
@@ -207,6 +207,7 @@ void ConvGpu::col2im(const Matrix &data_col, Vector &image)
                 }
                 else
                 {
+                    // int pick_idx = start_idx + (j / width_kernel) * width_in + j % width_kernel;
                     int pick_idx = cur_row * width_in + cur_col;
                     image(c * hw_in + pick_idx) += data_col(i, c * hw_kernel + j); // pick which pixel
                 }
@@ -224,6 +225,12 @@ void ConvGpu::backward(const Matrix &bottom, const Matrix &grad_top)
     grad_bottom.setZero();
     for (int i = 0; i < n_sample; i++)
     {
+
+        // Forward no longer needs the next 3 rows
+        Matrix data_col;
+        im2col(bottom.col(i), data_col);
+        data_cols[i] = data_col;
+
         // im2col of grad_top
         Matrix grad_top_i = grad_top.col(i);
         Matrix grad_top_i_col = Eigen::Map<Matrix>(grad_top_i.data(),
